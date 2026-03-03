80db54d662afea07291a76f226b237bb504198b8
153ys <daphnekao888@gmail.com>
2026-03-03 18:01:14 +0800

    refactor: 優化表單資料處理流程

--- src/main.js
+++ src/main.js
@@ -32,27 +32,32 @@ function pswValid(isLongEnough, rules) {
   return isLongEnough && rules >= 3;
 }
 
-psw.addEventListener("input", () => {
-  const { isLongEnough, rules } = getPswRules();
-
+function showPswError(isLongEnough, rules) {
   const errors = [];
   if (!isLongEnough) errors.push("密碼未滿 8 個字");
-  if (rules < 3) errors.push("密碼需包含大小寫英文字母以及 !@#$%^&*");
+  if (rules < 3) errors.push("需包含大、小寫字母、數字或特殊符號其中至少三種");
 
   pswError.textContent =
     errors.length > 0 ? `密碼格式不正確：${errors.join("、")}` : "";
+}
+
+psw.addEventListener("input", () => {
+  const { isLongEnough, rules } = getPswRules();
+  showPswError(isLongEnough, rules);
 });
 
 // 阻止預設行為與資料抓取
+
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("阻止成功！");
 
   const { isLongEnough, rules } = getPswRules();
 
   if (pswValid(isLongEnough, rules)) {
-    const formData = new FormData();
-    formData.append("email", email.value);
-    formData.append("password", psw.value);
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  } else {
+    showPswError(isLongEnough, rules);
   }
 });
